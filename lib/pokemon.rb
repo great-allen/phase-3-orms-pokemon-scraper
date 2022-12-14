8c0abcf7ee6d334343ae4b5472a606a87432b70d
QunFan <allenfan1012@gmail.com>
2022-12-14 14:43:09 +1100

    commit tree 89235ab8a2729c38dd6f45907b977122c52fee02

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,33 @@
 class Pokemon
+    attr_accessor :name,:type
+    attr_reader:id,:db
+    def initialize(id:,name:,type:,db:)
+        @id=id
+        @name=name
+        @type=type
+        @db=db
+    end
+
+    def self.save(name,type,db)
+        sql = <<-SQL
+          INSERT INTO pokemon (name, type)
+          VALUES (?, ?)
+        SQL
+    
+        db.execute(sql, name, type)
+    
+      end
+
+      
+      def self.find(id_num, db)
+        sql = <<-SQL
+            SELECT *
+            FROM pokemon
+            WHERE id = ?
+            SQL
+
+        db.execute(sql, id_num).map do |row|
+            self.new(id:row[0],name:row[1],type:row[2],db:db)
+        end.first
+    end
 end
